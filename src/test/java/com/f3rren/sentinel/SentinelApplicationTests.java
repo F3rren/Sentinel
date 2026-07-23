64bdb1bc75c6201ca80c2e8b76d0f3524c5a1845
TheF3rren <samueledisilvestri@gmail.com>
2026-07-23 15:12:01 +0200

    Initialize Maven project with Spring Boot setup, including wrapper scripts, POM file, and basic application structure.

---
+++ src/test/java/com/f3rren/sentinel/SentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f3rren.sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
